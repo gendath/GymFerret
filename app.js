8bf6683f1359831a25b1235bd0464b53c713fbeb
Perry Shelton <sellwithperry@gmail.com>
2021-08-08 21:50:41 -0700

    Initial Server Setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.set("views",path.join(__dirname,"views"))
+app.set("view engine","ejs")
+
+
+
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+
+
+
+
+
+app.listen(3000,()=>console.log("Server running on port 3000"))
